47fa42c73a40278b7c54c05cdf421b7c3dd1af28
savio <savionascimentodev@gmai.com>
2023-02-26 21:07:08 -0400

    ✨ add section 9, functions (not finished)

---
+++ 9-Funcoes/funcoes.php
@@ -0,0 +1,16 @@
+<?php 
+
+// Estrutura básica de uma função;
+function soma(){
+  // Código
+  echo 2 + 4 . '<br>';
+}
+
+// Chamando/Invocando a função;
+soma();
+
+// ------------------------------------------------------- //
+
+// Função do PHP que transforma strings em letras maiúsculas;
+echo strtoupper('ola mundo');
+?>
